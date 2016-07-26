ecd8f51b323a71832cec032cb6dfe96e9ae39cf7
Ivan Nikitin <vantoozz@gmail.com>
2016-07-26 11:32:53 +0300

    initial commit

---
+++ breathalyzer.php
@@ -0,0 +1,111 @@
+<?php
+
+$start = microtime(true);
+
+$inputFile = new SplFileObject($argv[1]);
+$inputFile->setFlags(SplFileObject::DROP_NEW_LINE);
+$inputText = explode(' ', mb_strtoupper($inputFile->fgets()));
+$inputFile = null;
+
+$vocabulary = [];
+$lengths = [];
+
+$file = new SplFileObject('vocabulary.txt');
+$file->setFlags(SplFileObject::DROP_NEW_LINE);
+
+while (!$file->eof()) {
+    $line = $file->fgets();
+    $length = mb_strlen($line);
+    $vocabulary[$length][$line] = $line;
+    $lengths[$length] = $length;
+}
+
+$file = null;
+
+$totalDistance = 0;
+$lengthsCount = count($lengths);
+
+$cache = [];
+
+foreach ($inputText as $inputWord) {
+    if ('' === $inputWord) {
+        continue;
+    }
+
+    /** @noinspection UnSafeIsSetOverArrayInspection */
+    if (isset($cache[$inputWord])) {
+        $totalDistance += $cache[$inputWord];
+        continue;
+    }
+
+    $wordLength = mb_strlen($inputWord);
+    /** @noinspection UnSafeIsSetOverArrayInspection */
+    if (isset($vocabulary[$wordLength][$inputWord])) {
+        continue;
+    }
+
+    $amplitude = 1;
+    $lengthsToCheck = $lengthsCount;
+    $odd = true;
+    $searchLength = $wordLength;
+    $min = PHP_INT_MAX;
+
+    do {
+        if ($min <= abs($wordLength - $searchLength)) {
+            break;
+        }
+
+        if (isset($vocabulary[$searchLength])) {
+            $minDistance = findMinDistance($vocabulary[$searchLength], $inputWord);
+            if ($minDistance < $min) {
+                $min = $minDistance;
+                if (1 === $min) {
+                    break;
+                }
+            }
+            $lengthsToCheck--;
+        }
+
+        if ($odd) {
+            $searchLength = $wordLength - $amplitude;
+        } else {
+            $searchLength = $wordLength + $amplitude;
+            $amplitude++;
+        }
+        $odd = !$odd;
+
+    } while ($lengthsToCheck);
+
+    $cache[$inputWord] = $min;
+
+    $totalDistance += $min;
+}
+
+if (isset($argv[2])) {
+    echo memory_get_peak_usage(true) . "\n";
+    echo microtime(true) - $start . "\n";
+}
+
+echo $totalDistance . "\n";
+
+/**
+ * @param array $vocabulary
+ * @param string $inputWord
+ * @return int
+ */
+function findMinDistance(array $vocabulary, $inputWord)
+{
+    $min = PHP_INT_MAX;
+
+    foreach ($vocabulary as $word) {
+        $distance = levenshtein($word, $inputWord);
+        if ($distance < $min) {
+            if (1 === $min) {
+                return $min;
+            }
+            $min = $distance;
+        }
+    }
+
+    return $min;
+}
